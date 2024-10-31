1f6d82be61285c0af6ae55079a6b98c52916ba09
meheraz1100 <meheraz1100@gmail.com>
2024-10-31 23:42:20 +0600

    basic date types : string, number, boolean, undefined, null, array, tuple,

---
+++ Module1/src/basicDataType.ts
@@ -0,0 +1,44 @@
+// basic data types
+
+
+// string
+
+let firstName : string = 'meheraz';   //explicite data type
+
+// number
+let roll : number = 123;
+
+// boolean
+let isAdmin : boolean = true;
+
+// undefined
+let x : undefined = undefined
+
+// null
+let y : null = null
+
+
+let d : number;
+
+d = 123
+// d = 'meheraz'
+// d = true
+
+
+
+// Array
+let friend : string[] = ['Rechel', 'Monika']
+friend.push()
+
+
+let eligibleRollList : number[] = [1,23,45,]
+eligibleRollList.push()
+
+
+// tuple --> array --> order --> type of values
+
+let coOrdinate : [number, number]= [1,5];
+
+let ageName : [number, string, boolean] = [50, 'mr x', true]
+
+ageName[1] = 'MR. y'
--- Module1/src/test.ts
+++ Module1/src/test.ts
@@ -1,6 +1,6 @@
 const test = "I am testing";
 
-let i = 0;
-for(;i < 10; i++){
-    console.log(i)
-}
+// let i = 0;
+// for(;i < 10; i++){
+//     console.log(i)
+// }
