3632ef2f887827ab364f86c5a2c4cc5b6ebaa3d2
meheraz1100 <meheraz1100@gmail.com>
2024-10-31 23:54:14 +0600

    learn about object data type, and readonly property

---
+++ Module1/src/objectType.ts
@@ -0,0 +1,16 @@
+// Refference type --> Object
+
+
+const user : {
+    readonly company: string   //types --> iterel types
+    firstName : string,
+    middleName?: string,  //optional type
+    lastName : string,
+    isMarried : boolean
+} = {
+    company : 'Facebook',
+    firstName : 'Mosaiyeb',
+    lastName : 'Meheraz',
+    isMarried : true
+}
+
