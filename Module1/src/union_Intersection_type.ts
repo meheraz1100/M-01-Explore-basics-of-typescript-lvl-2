de275077a748e4db0b9e7596775eeaafa2396009
meheraz1100 <meheraz1100@gmail.com>
2024-11-01 08:57:00 +0600

    Union and Intersection types

---
+++ Module1/src/union_Intersection_type.ts
@@ -0,0 +1,45 @@
+{
+
+// Union Types
+
+// type FrontendDeveloper = 'fakibazDeveloper' | 'juniorDeveloper'
+// type FullstackDeveloper = 'frontendDeveloper' | 'expertDeveloper'
+
+// type Developer = FrontendDeveloper | FullstackDeveloper
+
+
+// const newDeveloper : FrontendDeveloper = 'juniorDeveloper'
+
+// type User = {
+//     name: string,
+//     email: string,
+//     gender: 'male' | 'female',
+//     bloodGroup: 'O+' | 'A+' | 'A-' | 'AB+'
+// }
+
+// const user1: User = {
+//     name: 'Meheraz',
+//     email: 'mosaiyebmeheraz@gmail.com',
+//     gender: 'male',
+//     bloodGroup: 'A+'
+// }
+
+type FrontendDeveloper = {
+    skills: string[];
+    designation1: 'Frontend Developer'
+}
+
+type BackendDeveloper = {
+    skills: string[];
+    designation2: 'Backtend Developer'
+}
+
+type FullStackDeveloper = FrontendDeveloper & BackendDeveloper
+
+const fullstackDeveloper : FullStackDeveloper = {
+    skills: ['HTML', 'CSS', 'Express'],
+    designation1: 'Frontend Developer',
+    designation2: 'Backtend Developer'
+}
+
+}
