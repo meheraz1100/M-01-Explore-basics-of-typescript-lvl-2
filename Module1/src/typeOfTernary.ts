2f736062b385b555a88a9024fa0d3430ac69438d
meheraz1100 <meheraz1100@gmail.com>
2024-11-01 20:56:38 +0600

    Ternary, optional chaining & nullish coalescing operator

--- Module1/src/typeOfTernary.ts
+++ Module1/src/typeOfTernary.ts
@@ -12,7 +12,41 @@ if(age >= 18){
 }
 
 
+const isAdult = age >= 18 ? 'adult' : 'not adult'
+// console.log({isAdult})
 
+// nullish coalescing operator
+// null / undefined ---> decision making
+
+
+const isAuthenticated = '';
+
+const result1 = isAuthenticated ?? 'Guest'
+const result2 = isAuthenticated ? isAuthenticated : 'Guest'
+
+console.log({result1}, {result2});
+
+type User = {
+    name: string;
+    address: {
+        city: string;
+        road: string;
+        presentAddress: string;
+        permanentAddress?: string
+    }
+}
+
+const user: User = {
+    name: 'Meheraz',
+    address: {
+        city: 'feni',
+        road: 'Feni road',
+        presentAddress: 'Feni town'
+    }
+}
+
+const permanentAddress = user?.address?.permanentAddress ?? 'No Permanent Address'
+console.log({permanentAddress})
 
 
 
